3b140c184d31ae5aed2a60043448c5594e48b5c1
Kevin Nguyen <nguyenkevin1162@gmail.com>
2021-03-24 17:55:09 -0700

    Added enemy patrol and 3 new enemies

---
+++ 2D Platformer/Assets/Scripts/Enemy Scripts/EnemyPatrol.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFacingRight())
+        {
+            rb.velocity = new Vector2(moveSpeed, 0f);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-moveSpeed, 0f);
+        }
+    }
+
+    private bool isFacingRight()
+    {
+        return transform.localScale.x > Mathf.Epsilon;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        // Turn if sprite collides into a wall and flip sprite
+        transform.localScale = new Vector2(-(Mathf.Sign(rb.velocity.x)), transform.localScale.y);
+    }
+}
