e4e7fc40c723867b51b53c10baf029e7b5b289b8
Kevin Nguyen <nguyenkevin1162@gmail.com>
2021-04-17 16:18:16 -0700

    Added slime sounds

---
+++ 2D Platformer/Assets/Scripts/Enemy Scripts/Slime/SlimeAttack.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeAttack : MonoBehaviour
+{
+    public int attackDamage = 20;
+
+    public Vector3 attackOffset;
+    public float attackRange = 1f;
+    public LayerMask attackMask;
+
+    public AudioSource slimeSound1;
+    public AudioSource slimeSound2;
+    public AudioSource slimeSound3;
+
+    public void Attack()
+    {
+        Vector3 pos = transform.position;
+        pos += transform.right * attackOffset.x;
+        pos += transform.up * attackOffset.y;
+
+        Collider2D colInfo = Physics2D.OverlapCircle(pos, attackRange, attackMask);
+        if (colInfo != null)
+        {
+            colInfo.GetComponent<PlayerHealth>().TakeDamage(attackDamage);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Vector3 pos = transform.position;
+        pos += transform.right * attackOffset.x;
+        pos += transform.up * attackOffset.y;
+
+        Gizmos.DrawWireSphere(pos, attackRange);
+    }
+
+    public void slimeAttackSound1()
+    {
+        slimeSound1.Play();
+    }
+    public void slimeAttackSound2()
+    {
+        slimeSound2.Play();
+    }
+    public void slimeAttackSound3()
+    {
+        slimeSound3.Play();
+    }
+}
