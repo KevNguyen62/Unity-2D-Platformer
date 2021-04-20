fbdd7abc1f7a4c7735511064b100599fe3eb8eec
Kevin Nguyen <nguyenkevin1162@gmail.com>
2021-04-20 13:47:38 -0700

    Added player checkpoint system

---
+++ 2D Platformer/Assets/Scripts/Teleport/GameMaster.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    private static GameMaster instance;
+    public Vector2 lastCheckPointPosition;
+
+    void Awake()
+    {
+        // Store checkpoints so checkpoint data does not get resetted when a scene is reloaded
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
